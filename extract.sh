f7a9622266d5ff4420a27b17b36544fe3cdec09b
ryan1640 <ryan@nightvision.net>
2024-08-29 15:03:00 -0400

    Update extract.sh

--- extract.sh
+++ extract.sh
@@ -2,5 +2,5 @@ echo "nightvision swagger extract ./ -t javaspringvulny-api --lang spring"
 nightvision swagger extract ./ -t javaspringvulny-api --lang spring
 echo "==================================================================="
 read
-echo "nightvision swagger diff -n openapi-spec.yml -o openapi.yaml"
-nightvision swagger diff -n openapi-spec.yml -o openapi.yaml 
+echo "nightvision swagger diff openapi.yaml openapi-spec.yml "
+nightvision swagger diff openapi.yaml openapi-spec.yml 
